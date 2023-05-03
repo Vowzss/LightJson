3db6b69b5253ba5f40c57d93f020635300225120
Vowzss <dev@noideaindustry.com>
2023-05-03 09:42:24 +0200

    :sparkles: Changed JsonVirtual class for more flexibility

--- LightJson/Includes/JsonVirtual.h
+++ LightJson/Includes/JsonVirtual.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include "JsonUtils.h"
+
 namespace LightJson {
 	class JsonObject;
-
+	class JsonElement;
+	
 	class JsonVirtual
 	{
 	public:
-		virtual void serialize(JsonObject& jsonObject) = 0;
-		virtual void deserialize(JsonObject& jsonObject) = 0;
+		virtual ~JsonVirtual() = default;
+		
+		virtual JsonElement* Serialize()					   { return nullptr; }
+		virtual void		 Serialize(JsonObject& jsonObject) {}
+		
+		virtual void Deserialize(JsonObject& jsonObject)	  {}
+		virtual void Deserialize(JsonUtils::JsonMap& jsonMap) {}
 	};
 }
 
