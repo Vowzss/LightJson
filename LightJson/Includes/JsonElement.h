950f017d5b40ec8224136164ecc8ae41dcdd299a
Vowzss <dev@noideaindustry.com>
2023-05-03 09:40:10 +0200

    :sparkles: Implemented NumberElement new types + :zap: Better type assignment

--- LightJson/Includes/JsonElement.h
+++ LightJson/Includes/JsonElement.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <typeinfo>
 
 #include "JsonUtils.h"
 
@@ -18,11 +19,16 @@ namespace LightJson
         JsonElement() : type(JsonUtils::JsonType::Null) {}
         virtual ~JsonElement() = default;
     
-        virtual std::string          getAsString()  const { throw std::runtime_error("Element is not a string");  }
-        virtual double               getAsNumber()  const { throw std::runtime_error("Element is not a number");  }
-        virtual bool                 getAsBoolean() const { throw std::runtime_error("Element is not a boolean"); }
-        virtual JsonUtils::JsonArray getAsArray()   const { throw std::runtime_error("Element is not an array");  }
-        virtual JsonUtils::JsonMap   getAsObject()  const { throw std::runtime_error("Element is not an object"); }
+        virtual std::string          getAsString()  const { throw std::runtime_error("Element is not a string");   }
+        virtual bool                 getAsBoolean() const { throw std::runtime_error("Element is not a boolean");  }
+        virtual JsonUtils::JsonArray getAsArray()   const { throw std::runtime_error("Element is not an array");   }
+        virtual JsonUtils::JsonMap   getAsObject()  const { throw std::runtime_error("Element is not an object");  }
+        virtual float                getAsFloat()   const { throw std::runtime_error("Element is not a float");    }
+        virtual int                  getAsInteger() const { throw std::runtime_error("Element is not an integer"); }
+        virtual double               getAsDouble()  const { throw std::runtime_error("Element is not a double");   }
+        virtual long                 getAsLong()    const { throw std::runtime_error("Element is not a long");     }
+        virtual short                getAsShort()   const { throw std::runtime_error("Element is not a short");    }
+        virtual char                 getAsChar()    const { throw std::runtime_error("Element is not a char");     }
     
         bool isOfType(const JsonUtils::JsonType& _type) const { return type == _type; }
 
@@ -45,21 +51,42 @@ namespace LightJson
     }
 
 
-    class NumberElement final : public JsonElement
+    template <typename T> class NumberElement final : public JsonElement
     {
     private:
-        double value;
-    
+        T value;
+        
     public:
-        explicit NumberElement(const double& value);
-        double getAsNumber() const override { return value; }
+        explicit NumberElement(const T& value);
+        
+        float  getAsFloat()   const override { return (float)value; }
+        int    getAsInteger() const override { return (int)value;   }
+        double getAsDouble()  const override { return (double)value; }
+        long   getAsLong()    const override { return (long)value;   }
+        short  getAsShort()   const override { return (short)value;  }
+        char   getAsChar()    const override { return (char)value;   }
     };
-    inline NumberElement::NumberElement(const double& value) : value(value)
+    template <typename T> NumberElement<T>::NumberElement(const T& value) : value(value)
     {
-        type = JsonUtils::JsonType::Number;
+        switch (typeid(T).hash_code()) {
+            case typeid(int).hash_code():
+            case typeid(unsigned int).hash_code():
+            case typeid(size_t).hash_code():
+                type = JsonUtils::JsonType::Integer; break;
+            case typeid(float).hash_code():
+                type = JsonUtils::JsonType::Float; break;
+            case typeid(long).hash_code():
+                type = JsonUtils::JsonType::Long; break;
+            case typeid(short).hash_code():
+                type = JsonUtils::JsonType::Short; break;
+            case typeid(char).hash_code():
+                type = JsonUtils::JsonType::Char; break;
+            default:
+                type = JsonUtils::JsonType::Double; break;
+        }
     }
-
-
+    
+    
     class BooleanElement final : public JsonElement
     {
     private:
@@ -83,8 +110,8 @@ namespace LightJson
     public:
         explicit ArrayElement(JsonUtils::JsonArray array);
         ~ArrayElement() override {
-            for(const auto* it : array) {
-                delete it;
+            for(const JsonElement* element : array) {
+                delete element;
             }
             array.clear();
         }
