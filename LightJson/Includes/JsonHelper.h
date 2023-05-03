4d6a2389fbfb69ff359d151c2b059649d0291503
Vowzss <dev@noideaindustry.com>
2023-05-03 09:49:53 +0200

    :sparkles: Added JsonHelper for outer custom object class implementation

---
+++ LightJson/Includes/JsonHelper.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "JsonUtils.h"
+
+namespace LightJson
+{
+    class JsonHelper
+    {
+    public:
+        template <typename T> static JsonElement* Serialize(const T& value);
+        template <typename T> static T Deserialize(JsonUtils::JsonMap jsonMap);
+    };
+}
---
+++ LightJson/Sources/JsonHelper.cpp
@@ -0,0 +1,1 @@
+﻿#include "../Includes/JsonHelper.h"
